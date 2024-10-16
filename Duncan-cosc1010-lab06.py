61568909dc67d1541f946901bad38bee2b6055b3
Jduncan18 <jdunca18@uwyo.edu>
2024-10-16 21:30:54 +0000

    completed all sections

---
+++ Duncan-cosc1010-lab06.py
@@ -0,0 +1,138 @@
+# Jedidiah Duncan
+# UWYO COSC 1010
+# 10/15/24
+# Lab 06
+# Lab Section: 15
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+
+
+random_string = """
+jppamiqxegokaizvkyawwurhewtcxohryzptznyuedhhmawpic
+pkzwuiorngdfcsgqnlyifzyaivehpiyszykqprbcsobygzhadd
+yfddbulxmcnyvqhesmnybyuhxjqqmhdxwhcselasiayqhctnlw
+hakethjahqnvjdowhlyzosemxkbenestxgvgncmffkcxldcmkl
+itclmqdhrbdgzwtvdxwedcknbyaecvttjphtxubvhwvcvjqayy
+almxuxjbcmznnzekptfzbldsjwpvringlmalwufvlppeiendur
+dyophftqjkghhncwxoksqaqnpueudpygiytqgpcgjqsjbtbpzi
+vaeczmyicnednjjoxkpnjmpfbgyjnbfjlweqqppodfxfzzwkuf
+rldgryyhceuikimoavosuzuozthmatcgxcmkxnaxmsevkcumby
+spiajlbycvrluxdkfavxidzalxuixqkxiybhfuqhcvmrhzbzse
+idjwgwdwgfkyreozkyoxdvhixfejxjfgkkgobescboyfshiovu
+fxdyvfsnmjzsphgmtldlaoehofcspzujghcdcxzggvunpbtglr
+topplmkviuewwpoaplmbpgejmymxbyzzwbnujrlysszmxkjerb
+zpiewqvgopvhmmcgwcyvxvwhdvfgsrybcozhdtwujhdbxzznkc
+ergcqbetpgwrejuluqfxchlihunzbcdwboysjqenjxzbgqbycx
+dybxpyztjyxpkqfvxullzkedpkjjobhymfinpvprxejktyrpai
+ehjgwahpquzcmvclatdfcmattavoehnhnzveoxwnmnptxbvxto
+gpcobgzdhsjevhcohkltftmrqkosknkxeylhqxkkctbnusijgr
+uvecpbqmylqdaohkfaqbgeokyyipumjuaaayikdzyxfrpaieyo
+uxiosjwioebsjtslblfurgcodtyaggzovzfnnyjngawiwbbtqi
+kqqhnkwheolpqzasmsmbxqkeiqvogquobphewznfsnlkkizhca
+cbiyvxpmjxywqvzqtshfvnfbusphggexfqzepsrduvtovdsknl
+ztyuwugprkhbmktfvrenbmqgdjwnkeugtojrpqfmjhtrlcqcpq
+pwsguedzgvktpwbqkhkueymjtxbvzmdfjopzkygujrjdtogssg
+cxczryuqhhgjlpultkoffescpzyjrfqqabnhkfdnhkylpjamxk
+uxidjkqdrkxqjqjtflebvwhcvqjciykzhrvppvxhvpedgznwty
+kujglixooczrhxziasjxddfcghzlwrqcyiilpruhdfvitewxzg
+dzcvmvnoskchscgoqfsojfvahlwkrslzeirlblseplcmpmbmum
+ibrdamvqfstydtjopdkdcbnnmpifxckozyxzluhcqbqtpismog
+ulufaajxvuizvdzioxfvypxovptkibcrjvfidomejknuggfrtp
+kptwffersvqjknemkejsgspckwqisdcliuezhbeqpwgrjcqajl
+huobykkbujmyuuinbwdklqfhvakyozzsxghfyownjjwqtkxgkf
+ipdbjzxfogozstfsektujsvklrvecditiectuvtfibohmxxzna
+cpqzeoburtquuizhypugnkvuwbdxnraareqkofhfjobrpcsuxq
+nbafxlkuafbfsiuyrxdusqyasqyrwhdjrukgxdackumvairlgn
+fjhenwbrdghbevgqbybpwncclolgqyuhallbqtzdywbvlzwtil
+jctmsxjortnxvlbhuhkblppewjhqjzxrwgftlturxjuwfoaqpp
+sgfnxwxolkbrpdmpniitoljzaxabgtnelrmryetxqypwrjdyjc
+zipwbdpbazxpesmrcfuikeamtlsrgxrhzfytecenyydeemrhxj
+gmdruhillntvpadzbroyygydpmonwuakruvxbdrqhtrjvoqsin
+gjbarzvuqplmsmbwtqfghteoknbxmaokwlqqfoblmzsxczjzfj
+mzmawtarjdtgongqqufhhdjwcinhlxcsgoltjycxrkloqozxoi
+crlfmgflzzxgiiliqlksxyaydsohhahzxtsufzppftvgbpsdlx
+ertfmbothijzrrdvfrnsohnwulcxvcvxngvmznhazxrgdsugij
+fracotpirvqemsiuualpkpvtmtgchmowkmvoolrjfblrtwkmtr
+xhawucytgwlahddkhxxfublukkdldpovqokntydhzzrxiisdwu
+ujrkoewqoflyebogbwgdhriwkkoiofwtjlhxxtmzkklzbcmxhv
+lrslowamkcwolbcgfkfciegdwqskuazxnycqkkggzsowcmafay
+ibmkdwkqmdkjesqnjiqpijixbwjhenmsrrlpcseliiajlvcaac
+zkdenxczyooloczcaahnkehbwimvieedpdlqfafbqvxvfmvabd
+"""
+random_string = random_string.replace("\n","") #remove all newline characters
+print(len(random_string)) # Print out the size for reference 
+print(random_string)
+# Above is a string with 2500 characters.
+# Create a program that goes through and counts the occurrence of each character, excluding \n using a  dictionary
+# Output each letter and its corresponding occurrence in alphabetical order
+# Output which letter occurred the most 
+# Output which letter occurred the least 
+# Output what the percentage of the string each character is, again in alphabetical
+
+#Tips and trick:
+# You can iterate through strings like you would a list
+# All characters are lowercase 
+# Each letter will be PAIRED with its corresponding value 
+# That is to say, this is a great use of dictionaries
+    # You will  need to add the letter to the dictionary on first occurrence 
+    # Then increment its corresponding count 
+
+
+#Load all the elements into a dictionary
+#Will need to first declare a dictionary 
+
+
+####################################################################################################
+
+
+dictionary_of_occurrance = {}
+for letter in sorted(set(random_string)):
+    dictionary_of_occurrance[letter] = 0
+
+for letter in random_string:
+    dictionary_of_occurrance[letter] += 1
+
+for letter, value in dictionary_of_occurrance.items():
+    print(f"letter: {letter}")
+    print(f"value: {value}")
+    print(f"percent: {value/25}\n")
+
+max = 0
+max_letter = ()
+min = 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+min_letter = ()
+for letter, value in dictionary_of_occurrance.items():
+    if value > max:
+        max = value
+        max_letter = letter
+
+for letter, value in dictionary_of_occurrance.items():
+    if value < min:
+        min = value
+        min_letter = letter
+
+print(max_letter, max)
+print(dictionary_of_occurrance)
+
+
+
+##########################################################################################################
+
+
+# Output: each letter and its corresponding occurrence in alphabetical order
+
+print("*"*75)
+# Output which letter occurred the most 
+
+most_occurred = max_letter
+least_occurred = min_letter
+
+print(f"The letter that occurred the most is {most_occurred}")
+print("*"*75)
+# Output which letter occurred the least 
+print(f"The letter that occurred the most is {least_occurred}")
+print("*"*75)
+
+# Output what the percentage of the string each character is, again in alphabetical
